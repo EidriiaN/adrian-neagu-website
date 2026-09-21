35e920c0bb08c785a7e9399035de38064b976a65
EidriiaN <adrianneagu309@gmail.com>
2026-09-21 22:03:59 +0300

    feat: add Google Analytics gtag.js tracking

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Providers } from "./providers";
 
@@ -84,6 +85,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={inter.variable} translate="no">
       <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-EGVNT7DLK7"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-EGVNT7DLK7');
+          `}
+        </Script>
         <Providers>{children}</Providers>
       </body>
     </html>
